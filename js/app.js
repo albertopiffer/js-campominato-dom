4cf6ebe84be61e3a7734273e270cd5851ee75040
albertopiffer <albertopiffer96@gmail.com>
2023-02-12 19:53:43 +0100

    FEAT alert vittoria + rimosso event listener

--- js/app.js
+++ js/app.js
@@ -1,60 +1,92 @@
 const playButton = document.getElementById('playButton')
-playButton.addEventListener('click', function () {
+playButton.addEventListener('click', main)
 
-const grigliaX = parseInt(10)
-const grigliaY = parseInt(10)
+function main () {
 
-//console.log(grigliaX, grigliaY, grigliaX*grigliaY)
-
-let elGrid = document.querySelector('.griglia') //elementGrid
-
-elGrid.innerHTML = null //reset griglia
-
-for (let i=0; i<grigliaX*grigliaY; i++) {
-    elGrid.innerHTML += '<div class="cella" style="width: calc(100% / ' + grigliaX + ');" >' + parseInt(i+1) + '</div>'
+	const grigliaX = parseInt(10)
+	const grigliaY = parseInt(10)
+	
+	//console.log(grigliaX, grigliaY, grigliaX*grigliaY)
+	
+	let container = document.querySelector('.container') //elementGrid
+	let elGrid = document.querySelector('.griglia') //elementGrid
+	
+	elGrid.innerHTML = null //reset griglia
+	
+	for (let i=0; i<grigliaX*grigliaY; i++) {
+		elGrid.innerHTML += '<div class="cella" style="width: calc(100% / ' + grigliaX + ');" >' + parseInt(i+1) + '</div>'
+	}
+	
+	const elCells = document.querySelectorAll('.cella') //elementCells
+	
+	const posBombe = generaBombe(grigliaX, grigliaY, 16)
+	console.log(posBombe)
+	
+	let punteggio = 0
+	
+	let giaPremuto = []	//evita che punteggio incrementi se si preme ancora stessa cella
+	
+	for (let i=0; i<elCells.length ; i++) {	
+	
+		elCells[i].addEventListener('click', function () {
+			punteggio = CellsEL(i, elCells, posBombe, giaPremuto, punteggio, grigliaX, grigliaY, elGrid)
+		})
+	}	
 }
 
-const elCells = document.querySelectorAll('.cella') //elementCells
 
-const posBombe = generaBombe(grigliaX, grigliaY)
-console.log(posBombe)
-
-let punteggio = 0
-
-let giaPremuto = []	//evita che punteggio incrementi se si preme ancora stessa cella
-
-for (let i=0; i<elCells.length; i++) {
-	const cell = elCells[i]
-
-	cell.addEventListener('click', function () {
-		//console.log(i + 1)
-		if (!isPresent(i, posBombe)){
-			if (!isPresent(i, giaPremuto)) {
-				cell.style.backgroundColor = "rgba(0, 0, 255, 0.5)"
-				giaPremuto.push (i)
-				console.log (giaPremuto)
-				punteggio++
+function CellsEL(i, elCells, posBombe, giaPremuto, punteggio, grigliaX, grigliaY, elGrid) {
+	if (!isPresent(i, posBombe)){
+		if (!isPresent(i, giaPremuto)) {
+			elCells[i].style.backgroundColor = "rgba(0, 0, 255, 0.5)"
+			giaPremuto.push (i)
+			//console.log (giaPremuto)
+			punteggio++
+			if (punteggio == grigliaX*grigliaY-16) {
+				sendAlert(true, punteggio)
 			}
 		}
-		else {
-			cell.style.backgroundColor = "rgba(255, 0, 0, 0.7)"
-			console.log('BRUH')
-			console.log('punteggio: ', punteggio)
-		}
-		
-	})
+	}
+	else {
+		elCells[i].style.backgroundColor = "rgba(255, 0, 0, 0.7)"
+		console.log('punteggio: ', punteggio)
+
+		removeEL(posBombe, giaPremuto, punteggio, grigliaX, grigliaY, elGrid)
+
+		sendAlert(false, punteggio)
+	}
+	return punteggio
 }
 
-})
+function removeEL(pB, gP, p, X, Y, grid) {
+	
+	grid.innerHTML = null //reset griglia
+	
+	//console.log (gP)
+	//console.log (pB)
 
+	for (let i=0; i<X*Y; i++) {
+		grid.innerHTML += '<div class="cella" style="width: calc(100% / ' + X + ');" >' + parseInt(i+1) + '</div>'
+	}
 
+	const cells = document.querySelectorAll('.cella')
 
+	for (let i=0; i<X*Y; i++) {
 
-function generaBombe (x, y) {
+		if (isPresent(i, gP)) {
+			cells[i].style.backgroundColor = "rgba(0, 0, 255, 0.5)"
+		}
+		if (isPresent(i, pB)) {
+			cells[i].style.backgroundColor = "rgba(255, 0, 0, 0.7)"
+		}
+	}
+}
+
+function generaBombe (x, y, nB) {
 	let array = parseInt
 	array = []
 
-	while (array.length<16) {
+	while (array.length<nB) {
 		let rand = parseInt(Math.floor(Math.random()*(x*y)))
 		if (!isPresent(rand, array)){
 			array[array.length] = rand
@@ -74,5 +106,12 @@ function isPresent (a, b) {
 	return (check == 1)
 }
 
-//rgba(255, 0, 0, 0.7)
-//rgba(0, 0, 255, 0.5)
+function sendAlert(TF, p) {
+	
+	if(TF == false) {
+		alert('HAI PERSO!! PUNTEGGIO: ' + p)
+	}
+	else {
+		alert('HAI VINTO!!')
+	}
+}
